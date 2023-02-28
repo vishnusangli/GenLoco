bf859a950ab7440a69085055a6cace195ffc65a9
Vishnu Sangli <vsangli@berkeley.edu>
2023-02-28 02:54:18 -0800

    history steps reader

---
+++ motion_imitation/unify.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+
+# Convention for storing the info
+# Robot:
+# Frequency?:
+# Joints:
+# Stiffness:
+# Damping:
+
+
+# Data -->
+# Joints, Actuators
+
+class HistoryStepsAPI:
+    def __init__(self, robot_name, joint_names, stiffness=[], damping=[], freq=30) -> None:
+        self._robot_name = robot_name
+        self._joint_names = joint_names
+        self._stiffness = stiffness if len(stiffness)>0 else np.ones(len(joint_names))*100
+        self._damping = damping if len(damping)>0 else np.ones(len(joint_names))
+        self.curr_pointer=0
+        self.history=None
+        self.idx_num = len(joint_names)
+        self.begin_readings()
+    
+    def begin_readings(self, max_length=900):
+        self.history = np.zeros((max_length, len(self._joint_names)+len(self._joint_names)))
+        self.curr_pointer=0
+        self._max_length=max_length
+
+    def take_reading(self, joint_obs, motor_torque):
+        assert len(joint_obs) == len(self._joint_names)
+        assert len(motor_torque) == len(self._joint_names)
+        if self.curr_pointer >= self._max_length:
+            pass
+        else:
+            self.history[self.curr_pointer, :self.idx_num] = joint_obs
+            self.history[self.curr_pointer, self.idx_num:] = motor_torque
+            self.curr_pointer+= 1
+
+    def save_to_file(self, out_filename):
+        comments = "\n"
+        comments+=(f'Robot: {self._robot_name} \n')
+        comments+=(f'Frequency: ? \n')
+        comments+=(f'Joints {self._joint_names} \n')
+        comments+=(f'Stiffness: {self._stiffness} \n')
+        comments+=(f'Damping: {self._damping} \n')
+        comments+=(f'{self.curr_pointer} \n')
+
+        np.savetxt(out_filename, self.history, header=comments)
+    def load_from_file(self, in_filename):
+        self.history = np.loadtxt(in_filename)
+
+
+    def next(self):
+        if self.curr_pointer>= len(self.history):
+            return None, None
+        out = self.history[self.curr_pointer, :]
+        self.curr_pointer+=1 
+        return out[:self.idx_num], out[self.idx_num:]
+    
